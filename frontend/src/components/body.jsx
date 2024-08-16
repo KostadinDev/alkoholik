a3d30ed8058bcbe3c04bf23b21c1e9110fd0e83c
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2024-08-16 01:09:59 -0400

    add phone responsiveness

--- frontend/src/components/body.jsx
+++ frontend/src/components/body.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import DrinkCounter from './drink-counter';
-import { Button, Divider, Flex, Radio } from 'antd';
+import { Button } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
 
 function BodyComponent() {
@@ -26,9 +26,9 @@ function BodyComponent() {
   };
 
   return (
-    <div className="flex flex-col gap-10 items-center">
-      <span>{month}</span>
-      <div className="flex gap-5">
+    <div className="flex flex-col gap-6 items-center p-4">
+      <span className="text-lg font-semibold">{month}</span>
+      <div className="flex flex-wrap justify-center gap-4">
         {drinkCounters.map((counter, index) => (
           <DrinkCounter
             key={index}
@@ -42,7 +42,7 @@ function BodyComponent() {
         shape="round"
         icon={<PlusOutlined />}
         size="large"
-        className="w-[50%]"
+        className="w-full max-w-xs mt-4"
         onClick={() => incrementDrink('Ivan')} // Change to 'Kosta' if needed
       >
         Add Drink
