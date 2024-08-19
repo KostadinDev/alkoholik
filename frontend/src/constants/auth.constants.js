2fbb6f0fb9a7a2b82f02b157378f0e636f8af285
Kostadin Devedzhiev <kostadin.g.devedzhiev@gmail.com>
2024-08-18 21:58:38 -0400

    add boyan

--- frontend/src/constants/auth.constants.js
+++ frontend/src/constants/auth.constants.js
@@ -1,5 +1,6 @@
 
 export const kostadinEmail = 'kostadin.g.devedzhiev@gmail.com';
 export const ivanEmail = 'ivannikolov007@gmail.com';
+export const boyanEmail = 'boyan.zlatarski@gmail.com';
 
-export const EMAIL_WHITELIST = [kostadinEmail, ivanEmail];
+export const EMAIL_WHITELIST = [kostadinEmail, ivanEmail, boyanEmail];
